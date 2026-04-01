916cdf0239117515a15f65544a9fd26d3ec625da
AylieNagler <alapierrenagler@academic.rrc.ca>
2026-04-01 11:00:18 -0500

    Set up project structure with folders and empty files

---
+++ src/helpers.py
@@ -0,0 +1,8 @@
+"""
+Helper functions for [Your Project Name].
+
+This module contains reusable functions for data cleaning,
+analysis, and visualization.
+"""
+
+# You will add functions here starting in Week 13
